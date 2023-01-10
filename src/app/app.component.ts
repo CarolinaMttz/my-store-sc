0e118fda97f36ef79d9d4cfc1a5631e5ba49a8da
Dulce Carolina Martínez Carrera <dulce.martinez@ulsaoaxaca.edu.mx>
2023-01-10 12:59:38 -0600

    Clase Uso de Outputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   //imgParent = 'https://i.pinimg.com/736x/99/0a/95/990a95804d42ae98dcefcf5a7632b846.jpg';
   imgParent = '';
+
+  onLoaded(img: String){
+    console.log('log padre ', img);
+  }
+
 }
--- src/app/componenets/img/img.component.ts
+++ src/app/componenets/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -6,5 +6,21 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./img.component.scss']
 })
 export class ImgComponent {
-  @Input() img: string = 'valor init';
+
+  @Input() img: string = '';
+  @Output() loaded = new EventEmitter<String>();
+  imageDefault = './assets/images/default.png';
+
+
+
+  imgError(){
+    this.img = this.imageDefault;
+  }
+
+  imgLoaded(){
+    console.log('Log hijo');
+    this.loaded.emit(this.img);
+  }
+
+
 }
