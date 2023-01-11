1f0586e5a19fed849322661aa07c86358b717afc
Dulce Carolina Martínez Carrera <dulce.martinez@ulsaoaxaca.edu.mx>
2023-01-10 18:04:46 -0600

    Clase Obteniedo datos de una API

--- src/app/componenets/product/product.component.ts
+++ src/app/componenets/product/product.component.ts
@@ -10,9 +10,11 @@ export class ProductComponent {
 
   @Input() product: Product = {
     id: '',
-    name: '',
+    title: '',
+    price: 0,
     image: '',
-    price: 0
+    description: '',
+    category: ''
   }
 
   @Output() addedProduct = new EventEmitter<Product>();
--- src/app/componenets/products/products.component.ts
+++ src/app/componenets/products/products.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product.model';
 import { StoreService } from '../../services/store.service';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
 
   myShoppingCar: Product[] = [];
   total = 0;
 
+  /*
   products: Product[] = [
     {
       id: '1',
@@ -50,9 +52,23 @@ export class ProductsComponent {
       image: './assets/images/glasses.jpg'
     }
   ];
+  */
 
-  constructor( private storeService: StoreService ){
+  products: Product[] = [];
+
+  constructor(
+    private storeService: StoreService,
+    private productsService: ProductsService
+    ){
     this.myShoppingCar = this.storeService.getShoppingCar();
+  }
+
+  ngOnInit(): void {
+    this.productsService.getAllProducts()
+    .subscribe( data => {
+      console.log(data);
+      this.products = data;
+    });
   }
 
   onAddToShoppingCar(product: Product){
