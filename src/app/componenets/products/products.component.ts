23eed1b132a1079dee3536e58729be70f6533aab
Dulce Carolina Martínez Carrera <dulce.martinez@ulsaoaxaca.edu.mx>
2023-01-11 13:19:39 -0600

    Clase Implementando slides

--- src/app/componenets/products/products.component.ts
+++ src/app/componenets/products/products.component.ts
@@ -16,7 +16,17 @@ export class ProductsComponent implements OnInit{
   today = new Date();
   date = new Date(2020, 1, 23);
   showProductDetail = false;
-
+  productChoosen: Product = {
+    id: '',
+    title: '',
+    price: 0,
+    images: [],
+    description: '',
+    category: {
+      id: '',
+      name: ''
+    }
+  }
 
   constructor(
     private storeService: StoreService,
@@ -47,11 +57,13 @@ export class ProductsComponent implements OnInit{
 
 
   onShowDetail(id: string){
-      console.log("id => ",id);
+      //console.log("id => ",id);
       this.productsService.getProduct(id)
-      .subscribe(data => {
-          console.log("producto => ",data);
-      });
+                          .subscribe(data => {
+                              this.toogleProductDetails();
+                              //console.log("producto => ",data);
+                              this.productChoosen = data;
+                          });
   }
 
 }
