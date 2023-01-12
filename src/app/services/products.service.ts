c4a586d0821bbad649f7ee64d3c1102397900c54
Dulce Carolina Martínez Carrera <dulce.martinez@ulsaoaxaca.edu.mx>
2023-01-11 18:45:38 -0600

    Clase Reintentar una petición

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Product, CreateProductDTO, UpdateProductDTO } from '../models/product.model';
+import { retry, retryWhen} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  private apiUrl = 'https://young-sands-07814.herokuapp.com/api/products';
+  //private apiUrl = 'https://young-sands-07814.herokuapp.com/api/products';
+  private apiUrl = 'https://young-sands-07814.herokuappapp.com/api/products';
 
   constructor(
     private http: HttpClient
@@ -46,7 +48,9 @@ export class ProductsService {
   getProductsByPage(limit: number, offset: number){
     return this.http.get<Product[]>(this.apiUrl,{
         params: { limit, offset }
-    })
+    }).pipe(
+      retry(3)
+    );
   }
 
 }
