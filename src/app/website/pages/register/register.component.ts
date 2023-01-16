ef2bb7df5872d2b304727e3488c16f30aa394b94
Dulce Carolina Martínez Carrera <dulce.martinez@ulsaoaxaca.edu.mx>
2023-01-16 15:51:02 -0600

    Clase CanDeactivate

--- src/app/website/pages/register/register.component.ts
+++ src/app/website/pages/register/register.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { onExit } from './../../../guards/exit.guard';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements onExit {
+
+  onExit(){ //esta es una lógica de guardian desde el componente
+    const rta = confirm('Lógica de componente: ¿Está seguro que desea salir?');
+    return rta;
+  }
 
 }
--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -10,6 +10,7 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 import { LayaoutComponent } from './componenets/layaout/layaout.component';
 import { AuthGuard } from './../guards/auth.guard';
+import { ExitGuard } from './../guards/exit.guard';
 
 const routes: Routes = [
   {
@@ -46,6 +47,7 @@ const routes: Routes = [
       },
       {
         path: 'register',
+        canDeactivate: [ExitGuard],
         component: RegisterComponent
       },
       {
